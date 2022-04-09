61936c9d3edb132bf1fb50d98f6f9a6814678e1e
Serafs <serafsprogramming@gmail.com>
2022-04-09 22:34:34 +0300

    error response

--- server/src/api/api.go
+++ server/src/api/api.go
@@ -1,8 +1,15 @@
 package api
 
-import "github.com/gorilla/mux"
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
-type API struct{}
+type API struct {
+	logger *log.Logger
+}
 
 func (a *API) RegisterRoutes(r *mux.Router) {
 	api := r.PathPrefix("/api").Subrouter()
@@ -10,4 +17,20 @@ func (a *API) RegisterRoutes(r *mux.Router) {
 
 	api.HandleFunc("/register", a.handleRegister).Methods("POST")
 	api.HandleFunc("/login", a.handleLogin).Methods("POST")
+}
+
+func (a *API) errorResponse(w http.ResponseWriter, api string, code int, message string, sourceError error) {
+	a.logger.Error("API ERROR",
+		a.logger.Int("code", code),
+		a.logger.Err(sourceError),
+		a.logger.String("msg", message),
+		a.mlog.String("api", api),
+	)
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(model.ErrorResponse{Error: message, ErrorCode: code})
+	if err != nil {
+		data = []byte("{}")
+	}
+	w.WriteHeader(code)
+	_, _ = w.Write(data)
 }

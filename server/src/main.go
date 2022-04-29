456531bfb7c76eb54911f6623981b179817755f1
dkuzhakhmetov <dkuzhakhmetov@beeline.kz>
2022-04-29 15:54:04 +0400

    clean unused code

--- server/src/main.go
+++ server/src/main.go
@@ -12,30 +12,3 @@ package main
 func main() {
 
 }
-
-//http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-//	conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-//
-//	for {
-//		// Read message from browser
-//		msgType, msg, err := conn.ReadMessage()
-//		if err != nil {
-//			return
-//		}
-//
-//		// Print the message to the console
-//		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-//
-//		// Write message back to browser
-//		if err = conn.WriteMessage(msgType, msg); err != nil {
-//			return
-//		}
-//	}
-//})
-//
-//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-//	fmt.Println("Here")
-//	http.ServeFile(w, r, "./templates/index.html")
-//})
-//
-//http.ListenAndServe(":8080", nil)

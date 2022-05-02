12d4c173ca0cd7522c521e381877ccdf33cba76a
serafsprogrammnig@gmail.com <serafsprogramming@gmail.com>
2022-05-02 15:39:10 +0400

    infrastructure issues resolved

--- server/src/go.mod
+++ server/src/go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.4.2
+	github.com/labstack/echo/v4 v4.7.2
 	github.com/stretchr/testify v1.7.0
 )
--- server/src/main.go
+++ server/src/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello word")
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+	})
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	//httpPort := os.Getenv("HTTP_PORT")
+	//if httpPort == "" {
+	//	httpPort = "8000"
+	//}
+	httpPort := "8000"
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
 }
